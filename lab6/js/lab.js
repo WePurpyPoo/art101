3bad990c012607fc4d9f7a561a9654fbc3875a69
WePurpyPoo <thewe32master@gmail.com>
2023-10-27 17:09:48 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Creating Objects and Arrays
+// Author: Joey Longo & Chase Croy-Perrett
+// Date: 27 Oct 2023
 
-// Constants
+// My Variables (ways of getting around)
+var myTransport = [
+    "bus ",
+    "car ",
+    "plane ",
+    "shoes ",
+    "scooter ",
+];
 
-// Functions
+//My Object (My Mom's Car)
+var myMainRide = {
+   make: "Honda" , 
+   model: "Civic", 
+   color: "Maroon", 
+   year: 2001, 
+   age: function() {
+      return 2023 - this.year;
+   }
+};
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Writes statistics of car and transport to webpage
+document.writeln("These are the pieces of equipment I use to get around! They are: " + myTransport + "</br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+document.writeln("My Swiggity-swoogity Main Ride: <pre>",
+JSON.stringify(myMainRide, null, '\t'), +'\n'+"The car isn't mine, it's main man Lego Yoda's. A college student couldn't afford this! </pre>");
 
-// let's get this party started
-main();
